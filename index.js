083858884359405ded6121a44aaa1b0cdbe9f52f
Victor Hugo <victorhugofny@gmail.com>
2021-05-31 14:59:35 -0300

    att

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express"); 
 const app = express(); //inciando express
 
-app.listen(8080,function(erro){
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.listen(8080,function(erro){ //iniciando servidor
     if (erro){
         console.log("erro");
     }
     else{
         console.log("servidor iniciado")
     }
-}) //iniciando servidor
+}) 
