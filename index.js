504915d24ba96431a88111d17989577c5e571278
Victor Hugo <victorhugofny@gmail.com>
2021-06-03 18:19:06 -0300

    att

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express"); 
 const app = express(); //inciando express
 
+app.use(express.static(__dirname + 'public'))
+
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname + "/html/index.html");
+    res.sendFile(__dirname + "/public/");
 });
 
 app.listen(8080,function(erro){ //iniciando servidor
