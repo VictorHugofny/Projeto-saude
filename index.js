93dd0d97da55396d66ed1204d725a5940e3a9e8b
Victor Hugo <victorhugofny@gmail.com>
2021-06-04 23:37:09 -0300

    update html server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express"); 
 const app = express(); //inciando express
 
-app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/");
