4b67cc5f737728d4f02727c9e2d2b52b99f5a3a9
Victor Hugo <victorhugofny@gmail.com>
2021-06-14 17:00:44 -0300

    uptade

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express"); 
 const app = express(); //inciando express
+const bodyParser = require ('body-parser')
 
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname + '/public'))
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/");
 });
+
+app.post('/teste',(req,resp) =>{
+    console.log(req.body)
+    resp.send('Teste Feito')
+})
 
 app.listen(8080,function(erro){ //iniciando servidor
     if (erro){
